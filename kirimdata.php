a1fa3cd5cb2afab53c6380fc62494cb741981e9f
TegarSubagdja <113882965+TegarSubagdja@users.noreply.github.com>
2023-06-28 11:06:32 +0700

    Update kirimdata.php

--- kirimdata.php
+++ kirimdata.php
@@ -1,19 +1,20 @@
 <?php
-$koneksi = mysqli_connect("localhost", "", "", "");
+$koneksi = mysqli_connect("localhost", "username", "password", "database");
 
 if (mysqli_connect_errno()) {
     echo "Koneksi database gagal : " . mysqli_connect_errno();
 }
 
-if (isset($_GET['ldr'])) {
-    $ldr = $_GET['ldr'];
+if (isset($_GET['pir'])) {
+    $pir = $_GET['pir'];
     $datetime = date('Y-m-d H:i:s');
-    $simpan = mysqli_query($koneksi, "UPDATE `tb_ldr` SET `ldr`='$ldr', `date`='$datetime' WHERE `id`='1'");
+    $simpan = mysqli_query($koneksi, "UPDATE `tabel_sensor` SET `sensor`='$pir', `date`='$datetime' WHERE `id`='1'");
 
-    if ($simpan)
-        echo "Berhasil di Simpan";
-    else
-        echo "Gagal di Simpan";
+    if ($simpan){
+        echo "Berhasil disimpan";
+    } else {
+        echo "Gagal disimpan";
+    }
 } else {
     echo "Variabel ldr tidak didefinisikan";
 }
