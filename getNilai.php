3b3b0622b6f5dd638e97d744dc9f852aee47d779
TegarSubagdja <113882965+TegarSubagdja@users.noreply.github.com>
2023-06-28 11:05:51 +0700

    Update getNilai.php

--- getNilai.php
+++ getNilai.php
@@ -1,16 +1,16 @@
 <?php
 //koneksi ke database
-$koneksi = mysqli_connect("localhost", "", "", "");
+$koneksi = mysqli_connect("localhost", "username", "password", "database");
 
 //query untuk menampilkan data LDR dengan id=1
-$query = "SELECT * FROM tb_ldr WHERE id=1";
+$query = "SELECT * FROM tabel_sensor WHERE id=1";
 
 //eksekusi query
 $result = mysqli_query($koneksi, $query);
 
 //looping untuk menampilkan data LDR
 while ($row = mysqli_fetch_assoc($result)) {
-    echo "<tr><td class='align-middle text-center'>" . $row['ldr'] . "</td><td class='align-middle text-center'>" . $row['date'] . "</td></tr>";
+    echo "<tr><td class='align-middle text-center'>" . $row['sensor'] . "</td><td class='align-middle text-center'>" . $row['date'] . "</td></tr>";
 }
 
 //tutup koneksi ke database
